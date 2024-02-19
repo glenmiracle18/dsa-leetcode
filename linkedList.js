5b23941511c168bdaf20ea4d359318cbe08fd34c
glenmiracle18 <m.bonyu@alustudent.com>
2024-02-19 16:46:30 +0200

    done: finally working code

--- linkedList.js
+++ linkedList.js
@@ -1,87 +1,90 @@
-// this is the implementation of a list node and a linked list
-
+// this is the implementation of  alist node and linked list
 // node class
+
 class ListNode {
   constructor(data) {
-    this.data = data
-    this.next = null
+    this.data = data;
+    this.next = null;
   }
 }
 
 // list class
 class LinkedList {
   constructor(head = null) {
-    this.head = head
+    this.head = head;
   }
-}
 
+  // this contructor accepts multiple listNode arguments and link them
+  append(...nodes) {
+    let current;
 
-// this constructor accepts multiple listNode arguments and link them
-append(...nodes) {
-  if (!this.head) {
-    this.head = nodes[0]; // set head to the first node
-  } else {
-    let current = this.head;
-    while (current.next) {
-      current = current.next; // traverse to the last node
+    // if the list is not empty, set the head to the first node
+    if (!this.head) {
+      this.head = nodes[0];
+      current = this.head;
+    } else {
+    // otherwise, start from the last node
+      current = this.head;
+      while (current.next) {
+        current = current.next
+      }
     }
-    current.next = nodes[0]; // link last node to the first of new nodes
-    for (let i = 1; i < nodes.length; i++) { //link the remaining nodes
-      current.next = nodes[i]
+
+    // link the remaining nodes
+    for (let i = 0; i < nodes.length; i++) {
+      current.next = nodes[i];
       current = current.next;
     }
   }
-}
 
-// helper functions
-
-// get the size of the linkelist
-size() {
-  let count = 0;
-  let node  = this.head;
-  while (node) {
-    count ++;
-    node = node.next
-  }
-  return count;
-}
-
-// empties the list
-clear() {
-  this.head = null
-}
-
-// returns the last node of the linkedList
-getLast (); {
-  let lastNode = this.head;
-  if (lastNode) {
-    while (lastNode.next) {
-      lastNode = lastNode.next
+  // get the size of the list
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count ++;
+      node = node.next;
     }
+    return count;
   }
-  return lastNode;
+
+  // empties the list
+  clear () {
+    this.head = null;
+  }
+
+  // returns the last node of the linkedList
+  getLast() {
+    let lastNode = this.head;
+    if (lastNode) {
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+    }
+    return lastNode;
+  }
+
+  // returns the first node of the linked list
+  getFirst() {
+    return this.head;
+  }
 }
 
+  // create the linked list with multiple nodes
+  let node1 = new ListNode(2);
+  let node2 = new ListNode(5);
+  let node3 = new ListNode(7);
+  let node4 = new ListNode(14);
 
-// returns the first node of the linkedList
-getFirst() {
-  return this.head
-}
+  // creation of the linkedList
+  let list = new LinkedList();
+  list.append(node1, node2, node3, node4); // link the nodes together
 
-// create the linked list with multiple nodes
-let node1 = new ListNode(2);
-let node2 = new ListNode(5);
-let node3 = new ListNode(9);
-
-
-// creation of the linkedList
-let list = new LinkedList();
-list.append(node, node2, node3); // link the nodes together
-
-console.log(list.head.data) // return 2
-console.log(size())
-console.log(geLast())
-console.log(getFirst())
+  //logs
+  console.log(list.head.data);
+  console.log(list.size());
+  console.log(list.getLast());
+  console.log(list.getFirst());
 
 
 
