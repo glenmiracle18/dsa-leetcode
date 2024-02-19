bd4b3fa2fed077a8ffc21a6e882f92cc3902bc0b
glenmiracle18 <m.bonyu@alustudent.com>
2024-02-19 16:14:19 +0200

    update: parsed just the first node

--- linkedList.js
+++ linkedList.js
@@ -15,14 +15,23 @@ class LinkedList {
   }
 }
 
-// putting it alltogether
-let node1 = new ListNode(2)
-let node2 = new ListNode(5)
-let node3 = new ListNode(9)
 
-node1.next = node2
-node2.next = node3
-
+// this constructor accepts multiple listNode arguments and link them
+append(...nodes) {
+  if (!this.head) {
+    this.head = nodes[0]; // set head to the first node
+  } else {
+    let current = this.head;
+    while (current.next) {
+      current = current.next; // traverse to the last node
+    }
+    current.next = nodes[0]; // link last node to the first of new nodes
+    for (let i = 1; i < nodes.length; i++) { //link the remaining nodes
+      current.next = nodes[i]
+      current = current.next;
+    }
+  }
+}
 
 // helper functions
 
@@ -38,12 +47,12 @@ size() {
 }
 
 // empties the list
-clear () {
+clear() {
   this.head = null
 }
 
 // returns the last node of the linkedList
-getLast () {
+getLast (); {
   let lastNode = this.head;
   if (lastNode) {
     while (lastNode.next) {
@@ -59,12 +68,17 @@ getFirst() {
   return this.head
 }
 
+// create the linked list with multiple nodes
+let node1 = new ListNode(2);
+let node2 = new ListNode(5);
+let node3 = new ListNode(9);
 
 
 // creation of the linkedList
-let list = new LinkedList(node1)
-console.log(list.head.data) // return 2
+let list = new LinkedList();
+list.append(node, node2, node3); // link the nodes together
 
+console.log(list.head.data) // return 2
 console.log(size())
 console.log(geLast())
 console.log(getFirst())
