9f32292167b68ded54fceaaf5881c6208b58e4ea
glenmiracle18 <m.bonyu@alustudent.com>
2024-05-14 11:55:35 +0200

    build: project starts

---
+++ LinkedList/linkedList.js
@@ -0,0 +1,88 @@
+// this is the implementation of  alist node and linked list
+// node class
+
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// list class
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  // this contructor accepts multiple listNode arguments and link them
+  append(...nodes) { //pass in the nodes to be appended as args
+    let current;
+
+    // if the list is not empty, set the head to the first node. the nodes are the props of the list
+    if (!this.head) {
+      this.head = nodes[0];
+      current = this.head;
+    } else {
+    // otherwise, start from the last node
+      // current = this.head;
+      while (current.next) {
+        current = current.next
+      }
+    }
+
+    // link the remaining nodes
+    for (let i = 0; i < nodes.length; i++) {
+      current.next = nodes[i];
+      current = current.next;
+    }
+  }
+
+  // get the size of the list
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count ++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  // empties the list
+  clear () {
+    this.head = null;
+  }
+
+  // returns the last node of the linkedList
+  getLast() {
+    let lastNode = this.head;
+    if (lastNode) {
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+    }
+    return lastNode;
+  }
+
+  // returns the first node of the linked list
+  getFirst() {
+    return this.head;
+  }
+}
+
+  // create the linked list with multiple nodes
+  let node1 = new ListNode(2);
+  let node2 = new ListNode(5);
+  let node3 = new ListNode(7);
+  let node4 = new ListNode(14);
+
+  // creation of the linkedList
+  let list = new LinkedList();
+  list.append(node1, node2, node3, node4); // link the nodes together
+
+  //logs
+  console.log(list.head.data);
+  console.log(list.size());
+  console.log(list.getLast());
+  console.log(list.getFirst());
+
