31ca7433a41ce3137e8b75fab14fc2983565e939
glenmiracle18 <m.bonyu@alustudent.com>
2024-05-15 15:08:22 +0200

    refactor: circular-queue

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -2,4 +2,31 @@
 // as we all know, a linked list contain of a node that contains a pointer
 // that point to the element of the next node
 
+// node class
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
+// LinkedList
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+// call
+const list = new LinkedList();
+console.log("List is empty", list.isEmpty());
+console.log("List Size: ", list.getSize());
