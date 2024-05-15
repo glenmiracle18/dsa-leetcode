fd0c6c42a817b004eb78c424a5f06b2579edf2e4
glenmiracle18 <m.bonyu@alustudent.com>
2024-05-15 12:30:53 +0200

    refactor: queue.js

--- Queue/queue.js
+++ Queue/queue.js
@@ -3,25 +3,21 @@
 
 class Queue {
   constructor() {
-    this.items = []; // the queue array
+    this.items = [];
   }
 
-  // add elements to the queue
-  enqueue(element) {
-    this.items.push(element);
-  }
-
-  // remove the first item of the queue
-  dequeue() {
-    this.items.shift();
-  }
-
-  // return true if the queue is empty
   isEmpty() {
     return this.items.length === 0;
   }
 
-  // returns the first item of the queue
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
   peek() {
     if (!this.isEmpty()) {
       return this.items[0];
@@ -29,30 +25,19 @@ class Queue {
     return null;
   }
 
-  // return the number of items in the queue
-  size() {
-    return this.items.length;
-  }
-
-  // prints the overall queue
   print() {
-    console.log(this.items.toString());
+    console.log(this.items);
   }
 }
 
-// function/class call section
+// call
+
 const queue = new Queue();
 
-queue.enqueue(10);
 queue.enqueue(15);
-queue.enqueue(23);
-queue.enqueue(44);
-
-console.log(queue.isEmpty());
-queue.print();
-// console.log(queue.peek());
-// console.log(queue.size());
-//
-
-queue.dequeue();
-queue.print();
+queue.enqueue(16);
+queue.enqueue(17);
+queue.enqueue(18);
+// queue.print();
+queue.isEmpty();
+queue.peek();
